088e107408d0f63cd009a1fd27e638286f290bb2
@dinelkalakshan150 <dinelkalakshan150@gmail.com>
2026-01-12 16:13:48 +0530

    set up logging for product-service

--- product-service/src/main/java/lk/ijse/productservice/controller/ProductController.java
+++ product-service/src/main/java/lk/ijse/productservice/controller/ProductController.java
@@ -3,6 +3,8 @@ package lk.ijse.productservice.controller;
 import lk.ijse.productservice.dto.ProductDTO;
 import lk.ijse.productservice.entity.Product;
 import lk.ijse.productservice.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +13,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     private ProductService productService;
 
     @PostMapping
     public ProductDTO saveProduct(@RequestBody ProductDTO productDTO) {
+        log.info("Added new Product");
+        log.warn("This is a warn example");
+        log.error("This is error example");
         return productService.saveProduct(productDTO);
     }
     @GetMapping
     public List<ProductDTO> getAllProducts() {
+        log.info("Fetching all products");
+        log.debug("Debug: getProducts called");
         return productService.getAllProducts();
     }
     @GetMapping("/{productId}")
